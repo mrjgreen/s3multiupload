fbdf56662bb0da1faf0a5612200f6f79229730cd
Joe Green <joe.green0991@gmail.com>
2013-08-09 08:58:28 +0100

    updated composer

--- src/S3MultiUpload/KeyStorageInterface.php
+++ src/S3MultiUpload/KeyStorageInterface.php
@@ -4,6 +4,10 @@ class S3KeyStorageKeyNotFoundException extends Exception{}
 
 interface KeyStorageInterface {
 	
+	public function put($multipart_id, $data);
 	
+	public function delete($multipart_id);
+	
+	public function get($multipart_id);
 	
 }
