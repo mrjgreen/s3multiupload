247d233e2af639c2e8770825603d52ace3161b31
Joe Green <joe.green0991@gmail.com>
2013-08-13 10:40:08 +0100

    tidied code and added some doc blocks

--- example.php
+++ example.php
@@ -24,9 +24,11 @@ switch ($_REQUEST['action']) {
 		$multipart_id = !empty($_REQUEST['uploadId']) ? 
 			
 			$_REQUEST['uploadId'] : 
-		
+
 			$s3->createMultipart($config['bucket'], uniqid());
 		
+		// We need to set the content type as this header is used to form the signed request - we have to make sure that whatever we send with the data matches this
+		// This should always be 'application/octet-stream' for chunked uploads with plupload
 		echo json_encode($s3->signMultipart($multipart_id, $_REQUEST['chunk'], $_REQUEST['size'], array('Content-Type' => 'application/octet-stream')));
 		break;
 	
--- src/S3MultiUpload/Client.php
+++ src/S3MultiUpload/Client.php
@@ -1,31 +1,46 @@
 <?php namespace S3MultiUpload;
 
 use Aws\S3\S3Client;
-use Aws\S3\Enum\CannedAcl;
-
 use S3MultiUpload\KeyStorage\KeyStorageInterface;
-
 use S3MultiUpload\KeyStorage\Exception\KeyNotFoundException;
 
 class Client {
 
+	/**
+	 * 
+	 * @var Aws\S3\S3Client 
+	 */
 	public $s3;
 	
+	/**
+	 *
+	 * @var S3MultiUpload\KeyStorage\KeyStorageInterface 
+	 */
 	public $key_storage;
 	
+	
 	public function __construct(S3Client $s3, KeyStorageInterface $key_storage){
+		
 		$this->s3 = $s3;
+		
 		$this->key_storage = $key_storage;
 	}
+
 	
-	public function createMultipart($bucket, $key, $acl = CannedAcl::AUTHENTICATED_READ) {
+	/**
+	 * Send a request to Amazon S3 to initiate a multipart upload
+	 * 
+	 * @param string $bucket
+	 * @param string $key
+	 * @param array $options
+	 * @return string
+	 */
+	public function createMultipart($bucket, $key, array $options = array()) {
 		
-		$response = $this->s3->createMultipartUpload(array(
+		$response = $this->s3->createMultipartUpload($options + array(
 			'Bucket' => $bucket,
 			'Key' => $key,
-			'ACL' => $acl,
 		));
-
 
 		$multipart_id = $response->getPath('UploadId');
 
@@ -34,7 +49,16 @@ class Client {
 		return $multipart_id;
 	}
 
-	public function signMultipart($multipart_id, $chunk, $headers = array()) {
+	
+	/**
+	 * 
+	 * @param string $multipart_id
+	 * @param int $chunk The current chunk number
+	 * @param array $headers
+	 * @return array An array containing the presigned url to "PUT" the data to, the uploadId
+	 * @throws KeyNotFoundException
+	 */
+	public function signMultipart($multipart_id, $chunk, array $headers = array()) {
 
 		if (!list($bucket,$key) = $this->key_storage->get($multipart_id)) {
 			throw new KeyNotFoundException('There is no upload in progress for key "' . $multipart_id . '"');
@@ -50,13 +74,20 @@ class Client {
 		));
 
 		return array(
-			'url' => $command->createPresignedUrl('+10 minutes'), // The data can simply be "PUT" at this url directly
-			'uploadId' => $multipart_id,
-			'key' => $key,
-			'bucket' => $bucket
+			'url' => $command->createPresignedUrl('+10 minutes'),
+			'uploadId' => $multipart_id
 		);
 	}
 
+	
+	/**
+	 * Calling `$this->s3->listParts($fileoptions);` is okay in general unless there are more than 1000 parts - amazon will only send back the manifest for 1000 parts at a time
+	 * With a chuink size of 5mb the file only needs to be >5GB for this to happen.
+	 * We call the function recursively to fetch the next set of parts until we have them all.
+	 * 
+	 * @param array $fileoptions
+	 * @return array
+	 */
 	private function listParts($fileoptions) {
 
 		$parts = $this->s3->listParts($fileoptions);
@@ -66,6 +97,14 @@ class Client {
 		return $parts['IsTruncated'] ? array_merge($parts['Parts'], $this->listParts($fileoptions)) : $parts['Parts'];
 	}
 
+	
+	/**
+	 *
+	 * @param string $multipart_id
+	 * @return mixed The Amazon S3 response
+	 * @throws KeyNotFoundException
+	 * @throws \S3MultiUpload\Exception	
+	 */
 	public function completeMultipart($multipart_id) {
 
 		if(!list($bucket,$key) = $this->key_storage->get($multipart_id)){
@@ -91,14 +130,19 @@ class Client {
 
 			$this->abortMultipartUpload($multipart_id);
 
-			$this->key_storage->forget($multipart_id);
-			
 			throw $e;
 		}
 
 		return $response;
 	}
 
+	
+	/**
+	 * Abort the multipart upload with amazon and remove the key from storage
+	 * 
+	 * @param string $multipart_id
+	 * @throws KeyNotFoundException
+	 */
 	public function abortMultipartUpload($multipart_id) {
 		
 		if(!list($bucket,$key) = $this->key_storage->get($multipart_id)){
@@ -110,6 +154,8 @@ class Client {
 			'Key' => $key,
 			'UploadId' => $multipart_id
 		));
+		
+		$this->key_storage->forget($multipart_id);
 	}
 
 }
