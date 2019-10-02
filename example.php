4703b778c0dceeb5a19fb037c21ad88ec4c68a54
Rimas Kudelis <rq@akl.lt>
2019-10-02 21:30:36 +0300

    Fixed the example so it at least seems to work

--- example.php
+++ example.php
@@ -29,7 +29,7 @@ switch ($_REQUEST['action']) {
 		
 		// We need to set the content type as this header is used to form the signed request - we have to make sure that whatever we send with the data matches this
 		// This should always be 'application/octet-stream' for chunked uploads with plupload
-		echo json_encode($s3->signMultipart($multipart_id, $_REQUEST['chunk'], $_REQUEST['size'], array('Content-Type' => 'application/octet-stream')));
+		echo json_encode($s3->signMultipart($multipart_id, $_REQUEST['chunk'], array('Content-Type' => 'application/octet-stream', 'Content-Length' => $_REQUEST['size'])));
 		break;
 	
 	case 'complete' :
